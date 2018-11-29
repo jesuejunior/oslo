5f516b4181e9caabbdb9d34d5290255ff0a96da4
dominik-zeglen <flesz3@o2.pl>
2018-11-29 11:41:58 +0100

    Add transition state to staff views modals

--- saleor/static/dashboard-next/staff/views/StaffList.tsx
+++ saleor/static/dashboard-next/staff/views/StaffList.tsx
@@ -5,7 +5,7 @@ import Messages from "../../components/messages";
 import Navigator from "../../components/Navigator";
 import { createPaginationState, Paginator } from "../../components/Paginator";
 import i18n from "../../i18n";
-import { maybe } from "../../misc";
+import { maybe, getMutationState } from "../../misc";
 import StaffAddMemberDialog, {
   FormData as AddStaffMemberForm
 } from "../components/StaffAddMemberDialog";
@@ -72,6 +72,11 @@ export const StaffList: React.StatelessComponent<StaffListProps> = ({
                             }
                           }
                         });
+                      const addTransitionState = getMutationState(
+                        addStaffMemberData.called,
+                        addStaffMemberData.loading,
+                        maybe(() => addStaffMemberData.data.staffCreate.errors)
+                      );
                       return (
                         <Paginator
                           pageInfo={maybe(() => data.staffUsers.pageInfo)}
@@ -100,6 +105,7 @@ export const StaffList: React.StatelessComponent<StaffListProps> = ({
                                 path={staffMemberAddUrl}
                                 render={({ match }) => (
                                   <StaffAddMemberDialog
+                                    confirmButtonState={addTransitionState}
                                     errors={maybe(
                                       () =>
                                         addStaffMemberData.data.staffCreate
